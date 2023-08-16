d707a9971f14684b219e6391ec08869f86513cbf
BohunD <bbogunn@gmail.com>
2023-08-16 18:59:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/electroapp/presenation/fragments/SignInFragment.kt
@@ -0,0 +1,32 @@
+package com.example.electroapp.presenation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.electroapp.R
+
+
+class SignInFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() =
+            SignInFragment()
+    }
+}
